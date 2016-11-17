2de306a1df7d4fd67585ded64ebae9eb77cfdb07
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2016-11-17 06:14:55 -0300

    #10 Documentation.
    We also changed the name of the constructor arguments. While writing the
    documentation, the `_store` suffix sounded quite redundant and bothersome to
    type.

--- confeitaria/static/store/aggregate.py
+++ confeitaria/static/store/aggregate.py
@@ -21,15 +21,53 @@ import os
 
 
 class AggregateStore(object):
+    """
+    ``AggregateStore`` just delegates the task of reading documents to two
+    other stores, a primary one and a secondary one. First, it requests the
+    content from the primary store; if this one cannot provide the content,
+    aggregate store asks the secondary store for the document.
 
-    def __init__(self, primary_store, secondary_store):
-        self.primary_store = primary_store
-        self.secondary_store = secondary_store
+    An example can make help understanding it. Consider the two fake stores
+    below::
+
+    >>> from confeitaria.static.store.fake import FakeStore
+    >>> store1 = FakeStore({'test.html': 'FIRST'})
+    >>> store2 = FakeStore({'test.html': 'SECOND', 'test2.html': 'SECOND TOO'})
+
+    They both provide a ``test.html`` document, but only the second one can
+    serve a ``test2.html``. We can give them to an aggregate store::
+
+    >>> store = AggregateStore(primary=store1, secondary=store2)
+
+    If then we request ``test.html`` from the aggregate store, we will get the
+    value from ``store1``, because it is the primary store and  can serve it::
+
+    >>> store.read('test.html')
+    'FIRST'
+
+    Now, if we request ``test2.html``, then we will get the value from the
+    secondary store ``store2`` because ``store1`` has no such document::
+
+    >>> store.read('test2.html')
+    'SECOND TOO'
+
+    Requesting a document that is not available in any of the two stores will
+    result in a ``ValueError`` exception::
+
+    >>> store.read('nofile.html')
+    Traceback (most recent call last):
+      ...
+    ValueError: Failed to read nofile.html. Reason: 'nofile.html'
+    """
+
+    def __init__(self, primary, secondary):
+        self.primary = primary
+        self.secondary = secondary
 
     def read(self, path):
         try:
-            content = self.primary_store.read(path)
+            content = self.primary.read(path)
         except Exception as e:
-            content = self.secondary_store.read(path)
+            content = self.secondary.read(path)
 
         return content
